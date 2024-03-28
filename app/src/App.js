c41964f014e065c6ef2df7cbb90a85168cf61dde
francesco.sapioli <francesco.sapioli@gmail.com>
2024-03-28 11:16:16 +0100

    deleted anythinf that was inside the folder app; copied all f iles from youtube-admin-dashboard; it does run, packages should be upgraded but no big deal

--- app/src/App.js
+++ app/src/App.js
@@ -14,6 +14,7 @@ import FAQ from "./scenes/faq";
 import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -37,6 +38,7 @@ function App() {
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
               <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
