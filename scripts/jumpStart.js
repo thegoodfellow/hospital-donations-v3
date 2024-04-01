aa7b52267dccf928b73eb86170cc9404748fafd9
francesco.sapioli <francesco.sapioli@gmail.com>
2024-04-01 11:24:49 +0200

    scripts modifed: jumpStart calls : deploy --> deploy the contract writeContractInfo --> write the address and the abi inside 2 files in a folder inside app/src, so the app can retrieve them from there...

--- scripts/jumpStart.js
+++ scripts/jumpStart.js
@@ -1,17 +1,17 @@
-
 const deploy = require('../scripts/deploy'); //returns a istance of the contract deployed
-
+const writeContractInfo = require('../scripts/writeContractInfo');
 
 
 async function main(){
     console.log("START - /scripts/jumpStart.js");
 
     const contract = await deploy();
-
     //console.log("JSON.stringify(contract): " + JSON.stringify(contract));
-
     const address = await contract.getAddress();
     console.log("/scripts/deploy returned a deployed contract with address: " + address);
+
+    writeContractInfo(address);
+
      
     console.log("END - /scripts/jumpStart.js");
 }
---
+++ scripts/writeContractInfo.js
@@ -0,0 +1,43 @@
+require('dotenv').config({ path: '../.env' })
+const ethers = require('ethers');
+//const hre = require("hardhat");
+
+const fs = require('fs');
+const addressPath = "../app/src/contractInfo/contractAddress.json";
+const abiPath = "../artifacts/contracts/HospitalDonationV6.sol/HealthCareToken.json";
+const abiFinalPath = "../app/src/contractInfo/abi.json";
+
+//it writes the abi and the address of the contract into a folder isnide the app folder 
+//so once the contract is deployed those infos can be used from the app
+
+async function writeContractInfo(address) {
+    console.log("START - scripts/writeContractInfo.js");
+
+    //to-do each time the function is called it overwrites the contractAdress file with the new address 
+    //to-do of the deployed contract
+    //to-do it coul make sense to check if it already exists an address..
+
+    //retrieve contract abi from artifact
+    let rawData = fs.readFileSync(abiPath);
+    let jsonData = JSON.parse(rawData); 
+    const abi = jsonData.abi;
+
+    //write contract abi to contractInfo/abi
+    rawData = fs.readFileSync(abiFinalPath);
+    jsonData = JSON.parse(rawData); 
+    jsonData.abi = abi;
+    fs.writeFileSync(abiFinalPath, JSON.stringify(jsonData, null, 2));
+    
+    //write contract address to contractInfo/contractAddress
+    rawData = fs.readFileSync(addressPath);
+    jsonData = JSON.parse(rawData); 
+    jsonData.address = address;
+    fs.writeFileSync(addressPath, JSON.stringify(jsonData, null, 2));
+
+
+    console.log("END - scripts/writeContractInfo.js");
+
+
+}
+
+module.exports = writeContractInfo;
