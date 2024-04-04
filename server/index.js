a43c8de004981fd760686726b36981284e5ad184
francesco.sapioli <francesco.sapioli@gmail.com>
2024-04-04 19:33:19 +0200

    created folder server created folder/index.js created folder/readDonations.js installed packages: cors, express

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require("cors");// Cross-Origin Resource Sharing
+
+//to-do read donations from blockchain
+//to-do read NFTs from blockchain
+
+//to-do read quite few things..might be useful later on
+
+//to-do use ethers or alchemy-sdk to read from contract??? both??
+
+const port = 1225; //to-do eventually save it somewhere else --> maybe .env
+
+const app = express();
+
+const clientUrl = "http://localhost:5001"; //to-do eventually save it somewhere else --> maybe .env
+//to-do for the time being is running on 5001 it seems i forgot to close toher app running on 3000, and so on eventually change
+//to-do force client to run always on same port
+
+app.use(cors({
+    origin: clientUrl,
+  }));
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
+  });
---
+++ server/readDonations.js
@@ -0,0 +1,9 @@
+//to-do read donations from contract
+//to-do read them from varibles stored on contract memory or from events?
+//to-do which one is cheaper/faster/easier?
+//to-do if i read from events got tu sum the amounts donated to decide which token, on storage already got the sum...
+
+//to-do for the time being HealthCareToken/Donation struct might be used only to see at which NFTs one is entiled
+//to-do as it gets setted to 0 once the NFT is claimed..consider canging the behaviour
+//to-do for a full list of the donations the events DonationRecevied might be accessed...
+
